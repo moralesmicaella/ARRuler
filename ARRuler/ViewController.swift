4c8f8c370c26c096d8732002e784dbfc8938c476
Micaella Morales <morales.micaella@gmail.com>
2022-01-08 02:57:40 -0800

    Add a red dot on the location of the screen that is tapped

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.delegate = self
         
+        sceneView.debugOptions = [.showFeaturePoints]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,4 +38,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            if let raycastQuery = sceneView.raycastQuery(from: touchLocation, allowing: .estimatedPlane, alignment: .any) {
+                let results: [ARRaycastResult] = sceneView.session.raycast(raycastQuery)
+                if let result = results.first {
+                    addDot(at: result)
+                }
+            }
+        }
+    }
+    
+    func addDot(at result: ARRaycastResult) {
+        let dotNode = SCNNode()
+        dotNode.position = SCNVector3(result.worldTransform.columns.3.x,
+                                      result.worldTransform.columns.3.y,
+                                      result.worldTransform.columns.3.z)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        let dot = SCNSphere(radius: 0.005)
+        dot.materials = [material]
+        
+        dotNode.geometry = dot
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+    }
 }
