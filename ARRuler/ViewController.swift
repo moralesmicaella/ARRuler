85f2f405b14079893f077b7cd33f41f0261f495e
Micaella Morales <morales.micaella@gmail.com>
2022-01-08 04:02:39 -0800

    Add a clear function to clear the nodes in the screen, and reduce radius of dot.

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +42,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotNodes.count >= 2 {
+            clearScreen()
+        }
+        
         if let touchLocation = touches.first?.location(in: sceneView) {
             if let raycastQuery = sceneView.raycastQuery(from: touchLocation, allowing: .estimatedPlane, alignment: .any) {
                 let results: [ARRaycastResult] = sceneView.session.raycast(raycastQuery)
@@ -49,6 +54,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 }
             }
         }
+    }
+    
+    func clearScreen() {
+        textNode.removeFromParentNode()
+        
+        for dotNode in dotNodes {
+            dotNode.removeFromParentNode()
+        }
+        dotNodes.removeAll()
     }
     
     func addDot(at result: ARRaycastResult) {
@@ -60,7 +74,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let material = SCNMaterial()
         material.diffuse.contents = UIColor.red
         
-        let dot = SCNSphere(radius: 0.0025)
+        let dot = SCNSphere(radius: 0.002)
         dot.materials = [material]
         
         dotNode.geometry = dot
@@ -90,7 +104,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         textGeometry.firstMaterial?.diffuse.contents = UIColor.red
         
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode.geometry = textGeometry
         textNode.position = SCNVector3(position.x / 2, position.y + 0.005, position.z - 0.1)
         textNode.scale = SCNVector3(0.005, 0.005, 0.005)
         
