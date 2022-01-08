738d15fee15a779d523e1d97f502c4bb02221b35
Micaella Morales <morales.micaella@gmail.com>
2022-01-08 02:35:40 -0800

    Initial Commit

---
+++ ARRuler/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ARRuler
+//
+//  Created by Micaella Morales on 1/8/22.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
